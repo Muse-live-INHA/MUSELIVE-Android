9764fc86754fd5b03fa905028ca5cd6a0169456f
yesxon <parkyeseon@gmail.com>
2023-12-22 17:46:05 +0900

    ui: 충전 결제 ui 제작

--- app/src/main/java/com/inhauniv/hackathon/ui/charge/ChargeActivity.kt
+++ app/src/main/java/com/inhauniv/hackathon/ui/charge/ChargeActivity.kt
@@ -1,4 +1,15 @@
 package com.inhauniv.hackathon.ui.charge
 
-class ChargeActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
+import com.inhauniv.hackathon.R
+import com.inhauniv.hackathon.databinding.ActivityHomeBinding
+import com.inhauniv.hackathon.ui.base.BaseActivity
+
+class ChargeActivity: BaseActivity<ActivityHomeBinding>(R.layout.activity_charge) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
