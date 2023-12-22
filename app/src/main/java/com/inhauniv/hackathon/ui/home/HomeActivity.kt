acb0000525ff53237618bec732924039915ed4e1
kimhyein <sdu07024@naver.com>
2023-12-22 14:45:45 +0900

    ui: home ui 제작

--- app/src/main/java/com/inhauniv/hackathon/ui/home/HomeActivity.kt
+++ app/src/main/java/com/inhauniv/hackathon/ui/home/HomeActivity.kt
@@ -1,4 +1,13 @@
 package com.inhauniv.hackathon.ui.home
 
-class HomeActivity {
+import android.os.Bundle
+import com.inhauniv.hackathon.R
+import com.inhauniv.hackathon.databinding.ActivityHomeBinding
+import com.inhauniv.hackathon.ui.base.BaseActivity
+
+class HomeActivity: BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 }
