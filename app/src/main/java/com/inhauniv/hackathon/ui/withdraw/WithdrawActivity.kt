281ec9d4e4bf55279f69511929319f103805a71c
yesxon <parkyeseon@gmail.com>
2023-12-22 19:10:34 +0900

    ui:출금 ui 제작

---
+++ app/src/main/java/com/inhauniv/hackathon/ui/withdraw/WithdrawActivity.kt
@@ -0,0 +1,12 @@
+package com.inhauniv.hackathon.ui.charge
+
+import android.os.Bundle
+import com.inhauniv.hackathon.R
+import com.inhauniv.hackathon.databinding.ActivityHomeBinding
+import com.inhauniv.hackathon.ui.base.BaseActivity
+
+class WithdrawActivity: BaseActivity<ActivityHomeBinding>(R.layout.activity_withdraw) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
