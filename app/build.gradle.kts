c80bea0d1dad0aab147eee1c221c32644d9b719f
kimhyein <sdu07024@naver.com>
2023-12-22 17:27:09 +0900

    feat: FCM 알림 기능 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -46,6 +47,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-messaging-ktx:23.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -92,7 +94,8 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.0.1")
 
     // firebase
-    implementation ("com.google.firebase:firebase-bom:32.7.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
 
     // Jsoup
     implementation ("org.jsoup:jsoup:1.13.1")
--- app/src/main/java/com/inhauniv/hackathon/ui/home/HomeActivity.kt
+++ app/src/main/java/com/inhauniv/hackathon/ui/home/HomeActivity.kt
@@ -1,7 +1,11 @@
 package com.inhauniv.hackathon.ui.home
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.inhauniv.hackathon.R
 import com.inhauniv.hackathon.databinding.ActivityHomeBinding
 import com.inhauniv.hackathon.domain.entity.PaymentDetail
@@ -14,11 +18,25 @@ class HomeActivity: BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        getFCMToken()
         initAdapter()
         initView()
         observe()
     }
 
+    private fun getFCMToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.e(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            Log.d(TAG, "FCM token : $token")
+        })
+    }
     private fun initAdapter() {
         binding.rvPaymentDetails.itemAnimator = null
         binding.rvPaymentDetails.adapter = adapter
